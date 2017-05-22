6b4b37270f068b2efcdd76b45143bf8e6272ce14
JureSenegacnik <jure.senegacnik@comtradesoftware.com>
2017-05-23 00:35:15 +0200

    Naloga4-beta

---
+++ Naloga4/app.ts
@@ -0,0 +1,56 @@
+import * as angular from 'angular';
+import {MainController} from './app/mainController';
+const myApp = angular.module('comtrade',[]).controller('MainController',MainController) ;
+
+
+/*
+let vnos = prompt("Vnesi stevilo!");
+let i:number;
+for ( i=1; i <= Number(vnos); i++)
+{
+    if (i%3 === 0 && i%5 === 0) console.log("fizzbuzz");
+    else if (i%3 === 0) console.log("fizz");
+    else if (i%5 === 0) console.log("buzz");
+    else console.log(i);
+}
+*/
+
+
+/*********************************************/
+/*
+let maxi=prompt ("Do koliko bos ugibal?");
+let tezavnost = prompt ("Izberi tezavnost! 1-easy, 2-medium, 3 -hard")
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let poskusov:number = 0;
+let nakljucna= getRandomInt(0,Number(maxi));
+let vnesel = prompt("Ugani steivilo:");
+let 
+do
+{
+
+    if (Number(vnesel)>nakljucna)
+    {
+        vnesel = prompt ("prevec! Se enkrat!");
+    }
+    else if (Number(vnesel)<nakljucna)
+    {
+        vnesel = prompt ("premalo! Se enkrat!");
+    }
+    else 
+    {
+        alert("bingo");
+        break;
+    }
+    poskusov++;
+    if ((Number(tezavnost) === 2 && poskusov > 10 ) || (Number(tezavnost) === 3 && poskusov > 5))
+    {
+        alert("zmanjkalo ti je poskusov");
+        break;
+    }
+}while(1);*/
+
+/*mihec.susnik@gmail.com*/ 
+
---
+++ Naloga4/app/mainController.ts
@@ -0,0 +1,106 @@
+export class MainController {
+    data = '';
+    timeslots = 'none';
+    static $inject = ['$q', '$http'];
+    reservation=[];
+    
+
+    constructor(private $q: ng.IQService, private $http: ng.IHttpService) {
+
+    }
+
+    getHttp() {
+        this.$http.get('http://smartninja.betoo.si/api/CMW/cars')
+            .then((success: any) => {
+                    this.data = success.data;
+                }, (error: any) => {
+                    console.log(error);
+                }
+            );
+        this.$http.get('http://smartninja.betoo.si/api/CMW/timeslots')
+            .then((success: any) => {
+                    this.timeslots = success.data;
+                }, (error: any) => {
+                    console.log(error);
+                }
+            );
+    }
+    postHttp(){
+         this.reservation = [
+        {
+            "timeslotId": "selectedRental",
+             "email":"this.timeslots.selectedRental.contactEmail" 
+        }];
+        this.$http.post('http://smartninja.betoo.si/api/CMW/reservations', this.reservation)
+            .then((success: any) => {
+                    console.log(success);
+                }, (error: any) => {
+                    console.log(error);
+                }
+            );
+
+
+    }
+    /*getSlots() {
+        this.$http.get('http://smartninja.betoo.si/api/CMW/timeslots')
+            .then((success: any) => {
+                    this.timeslots = success.data;
+                }, (error: any) => {
+                    console.log(error);
+                }
+            );
+            
+
+        // only one radio can be logically checked, don't check the rest
+        
+    }*/
+    
+
+
+    fire() {
+        let initialHttpRequest = this.getDummyPromise();
+
+        this.data = "Firing";
+
+        //  Chaining promises
+        initialHttpRequest.then((success: string) => {
+
+        //    this.data = success ;
+            return this.getDummyPromise();
+        }, (error: string) => {
+
+            return "Modified error";
+        }).then((success2: string) => {
+
+            this.data = "2. then: success (" + success2 + ')';
+
+        }, (error2: string) => {
+
+            this.data = "2. then: error (" + error2 + ')';
+        });
+
+
+    }
+
+
+    getDummyPromise(): ng.IPromise<string> {
+        let defer = this.$q.defer();
+
+        setTimeout(function () {
+            //  async
+            let random = Math.round(Math.random());
+
+            if (random) {
+                //  success
+                defer.resolve('Success');
+            }
+            else {
+                //  error
+                defer.reject('Error');
+            }
+
+        }, 1000);
+
+        return defer.promise;
+}
+}
